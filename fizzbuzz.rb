9e9c1a2aad4b241887bf46712bfe5669666b24e8
Will Figueiredo <will@wfigueiredo.com>
2018-08-17 01:45:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz"
-  else int % 3 && 5 == 0 # if the number int is divisible by 3 & 5
+  else int % 3 && % 5 == 0 # if the number int is divisible by 3 & 5
     "FizzBuzz"
   end
 end
