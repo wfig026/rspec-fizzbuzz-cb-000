be52625d01229b5255ca918228e4c6661999752e
Will Figueiredo <will@wfigueiredo.com>
2018-08-17 02:04:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,21 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz"
-  elsif int % 5 == 0 # if the number int is divisible by 5
-    "Buzz"
-  elsif int % 15 == 0 # if the number int is divisible by 3 & 5
-    "FizzBuzz"
-  else
-    # return nil
+  # if int % 3 == 0 # if the number int is divisible by 3
+  #   "Fizz"
+  # elsif int % 5 == 0 # if the number int is divisible by 5
+  #   "Buzz"
+  # elsif int % 15 == 0 # if the number int is divisible by 3 & 5
+  #   "FizzBuzz"
+  # else
+
+    if int % 5 == 0 and int % 3 == 0
+        puts "FizzBuzz"
+      elsif int % 5 == 0
+        puts "Buzz"
+      elsif int % 3 == 0
+        puts "Fizz"
+      else
+        # return nil
   end
 end
